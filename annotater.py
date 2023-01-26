6492f9b1ae8baa0a08478ed583ceed3d4e4197ff
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2023-01-27 00:18:32 +0700

    Initial commit.

---
+++ annotater.py
@@ -0,0 +1,69 @@
+import re
+from typing import List, Tuple, Optional, Any
+
+from txtmarker.factory import Factory
+
+
+class Annotater():
+
+    def annotate(self, labels: List[str], documents: List[str], input_file_path: str, output_file_path: str) -> List[Tuple[str, str]]:
+        highlights = []
+        for label, document in zip(labels, documents):
+            highlight = (label, document)
+            highlights.append(highlight)
+            
+        highlighter = Factory.create("pdf", self.formatter, 4)
+        highlighter.highlight(input_file_path, output_file_path, highlights)
+        
+        return highlights
+
+    def formatter(self, text):
+        """
+        Custom formatter that is passed to PDF Annotation method. This logic maps data cleansing logic in paperetl.
+
+        Reference: https://github.com/neuml/paperetl/blob/master/src/python/paperetl/text.py
+
+        Args:
+            text: input text
+
+        Returns:
+            clean text
+        """
+
+        # List of patterns
+        patterns = []
+
+        # Remove emails
+        patterns.append(r"\w+@\w+(\.[a-z]{2,})+")
+
+        # Remove urls
+        patterns.append(r"http(s)?\:\/\/\S+")
+
+        # Remove single characters repeated at least 3 times (ex. j o u r n a l)
+        patterns.append(r"(^|\s)(\w\s+){3,}")
+
+        # Remove citations references (ex. [3] [4] [5])
+        patterns.append(r"(\[\d+\]\,?\s?){3,}(\.|\,)?")
+
+        # Remove citations references (ex. [3, 4, 5])
+        patterns.append(r"\[[\d\,\s]+\]")
+
+        # Remove citations references (ex. (NUM1) repeated at least 3 times with whitespace
+        patterns.append(r"(\(\d+\)\s){3,}")
+
+        # Build regex pattern
+        pattern = re.compile("|".join([f"({p})" for p in patterns]))
+
+        # Clean/transform text
+        text = pattern.sub(" ", text)
+
+        # Remove extra spacing either caused by replacements or already in text
+        text = re.sub(r" {2,}|\.{2,}", " ", text)
+
+        # Limit to alphanumeric characters
+        text = re.sub(r"[^A-Za-z0-9]", "", text)
+
+        return text
+
+
+annotater = Annotater()
---
+++ document_conversion.py
@@ -0,0 +1,83 @@
+import uuid
+import pdfkit
+
+import pdfkit
+import pdfrw
+from pdfrw import PdfReader, PdfWriter
+from pyvirtualdisplay import Display
+import hashlib
+
+from pre_processor import pre_processor
+
+class DocumentConversion:
+    def __init__(self):
+        self.options = {
+            'page-size': 'Letter',
+            'margin-top': '0.25in',
+            'margin-right': '1.00in',
+            'margin-bottom': '0.25in',
+            'margin-left': '1.00in',
+        }
+    
+    def text_to_pdf(self, text: str, output_file_path: str) -> bytes:
+        return pdfkit.from_string(text, output_file_path, options=self.options)
+    
+    def web_to_pdf(self, url: str, output_file_path: str) -> bytes: 
+        return pdfkit.from_url(url, output_file_path, options=self.options)
+        
+    def file_to_pdf(self, input_file_path: str, output_file_path: str) -> bytes:
+        return pdfkit.from_file(input_file_path, output_file_path, options=self.options)
+    
+    def corpus_to_pdf(self, passage_search_request: dict) -> str:
+        passage_search_request_hash = hashlib.md5(str(passage_search_request).encode("utf-8")).hexdigest()
+        output_file_path = f"temp/output_{passage_search_request_hash}.pdf"
+        if passage_search_request["source_type"] == "text":
+            self.text_to_pdf(
+                text=pre_processor.textract(
+                    corpus=passage_search_request["corpus"],
+                    granularity=passage_search_request["granularity"]
+                ),
+                output_file_path=output_file_path
+            )
+        elif passage_search_request["source_type"] == "file":
+            self.file_to_pdf(
+                input_file_path=passage_search_request["corpus"],
+                output_file_path=output_file_path
+            )
+        elif passage_search_request["source_type"] == "web":
+            self.web_to_pdf(
+                url=passage_search_request["corpus"],
+                output_file_path=output_file_path
+            )
+        else:
+            raise ValueError(f"Source type {passage_search_request['source_type']} is not supported.")
+        
+        return output_file_path
+    
+    def split_pdf_page(self, start_page: int, end_page: int, input_file_path: str, output_file_path:str):
+        pdf_reader = PdfReader(input_file_path)
+        pdf_writer = PdfWriter(output_file_path)
+
+        for page_num in range(start_page - 1, end_page):
+            pdf_writer.addpage(pdf_reader.pages[page_num])
+
+        pdf_writer.write()
+
+        return output_file_path
+    
+    def file_bytes_to_pdf(self, file_bytes: bytes, output_file_path: str):
+        file_hash = hashlib.md5(file_bytes).hexdigest()
+        file_name = "{}.pdf".format(str(file_hash))
+        file_path = f"{output_file_path}/{file_name}"
+        with open(file_path, "wb") as f:
+            f.write(file_bytes)
+        return output_file_path
+    
+    def get_pdf_page_length(self, input_file_path: str) -> int:
+        pdf_reader = PdfReader(input_file_path)
+        pdf_page_length = len(pdf_reader.pages)
+        return pdf_page_length
+
+        
+
+document_conversion = DocumentConversion()
