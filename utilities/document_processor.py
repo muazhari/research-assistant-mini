fe40d92a637d1bea7a4d643beb5db50fb833f50e
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2023-05-08 12:16:58 +0700

    Refactor rename.

--- utilities/document_processor.py
+++ utilities/document_processor.py
@@ -39,12 +39,12 @@ class DocumentProcessor:
 
     def granularize(self, corpus: str, corpus_source_type: str, granularity: str) -> List[str]:
         if corpus_source_type in ["text"]:
-            extracted_corpus = self.segment(corpus, granularity)
+            granularized_corpus = self.segment(corpus, granularity)
         elif corpus_source_type in ["file", "web"]:
-            extracted_corpus = self.textract(corpus, granularity)
+            granularized_corpus = self.textract(corpus, granularity)
         else:
             raise ValueError(f"Source type {corpus_source_type} is not supported.")
-        return extracted_corpus
+        return granularized_corpus
 
     def windowize(self, corpus: List[str], window_size: int) -> List[Tuple[str, ...]]:
         return list(more_itertools.windowed(corpus, window_size))
@@ -61,11 +61,11 @@ class DocumentProcessor:
 
     def process(self, corpus: str, corpus_source_type: str, granularity: str,
                 window_sizes: List[int]) -> List[Document]:
-        extracted_corpus: List[str] = self.granularize(corpus, corpus_source_type, granularity)
+        granularized_corpus: List[str] = self.granularize(corpus, corpus_source_type, granularity)
 
         processed_documents_with_many_window = []
         for window_size in window_sizes:
-            windowed_corpus: List[Tuple[str, ...]] = self.windowize(extracted_corpus, window_size)
+            windowed_corpus: List[Tuple[str, ...]] = self.windowize(granularized_corpus, window_size)
             for index_window, content_window in enumerate(windowed_corpus):
                 document: Document = Document(
                     content=self.degranularize(content_window, granularity),
